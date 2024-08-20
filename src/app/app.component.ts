984df429ed62096c3ab7bdf92d29858bf3eb3d48
Pranab Bora <prnbbora4@gmail.com>
2024-08-20 20:23:41 +0530

    router created for all page and app component modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  title = 'tutorial-angular';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { BlogsComponent } from "./pages/blogs/blogs.component";
+import { NotFound404Component } from "./pages/not-found404/not-found404.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { BlogDetailsComponent } from "./pages/blog-details/blog-details.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { ReviewsComponent } from "./pages/reviews/reviews.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "blogs", component: BlogsComponent },
+  { path: "blogs/details/:id", component: BlogDetailsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "reviews", component: ReviewsComponent },
+  { path: "**", component: NotFound404Component },
+];
